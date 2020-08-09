7708a39d6917a0da16b1acfa9b741d6e7104e93d
wuchangping <w84814077@163.com>
2020-08-09 20:24:33 +0800

    home

--- src/pages/home/Home.ts
+++ src/pages/home/Home.ts
@@ -4,5 +4,26 @@ import { Vue, Component } from 'vue-property-decorator'
   components: { }
 })
 export default class Home extends Vue {
+  userName: string = '吴彦祖'
+  phoneNum: string = '18852951656'
 
+  // 呼叫联系人
+  doCallPhone() {
+    uni.makePhoneCall({
+      phoneNumber: this.phoneNum
+    });
+  }
+
+  // 保存联系人
+  doSavePhone() {
+    uni.addPhoneContact({
+      nickName: this.userName,
+      firstName: this.userName,
+      mobilePhoneNumber: this.phoneNum,
+      weChatNumber: 'wcp0923',
+      success: function () {
+        uni.showToast({ title: '保存成功' })
+      }
+    });
+  }
 }
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,149 @@
 <template>
-    <view class="mine-page">
-        Home
+    <view class="home-page-wrapper">
+        <view class="card-wrapper">
+            <view class="left-wrapper">
+                <view class="name">{{ userName|empty }}</view>
+                <view class="career">前端工程师</view>
+                <view class="phone">
+                    <image class="image-32" src="/static/icon/phone-gray.png"></image>
+                    <view class="text">{{ phoneNum|empty }}</view>
+                </view>
+                <view class="address">
+                    <image class="image-32" src="/static/icon/bussiness.png"></image>
+                    <view class="text">上海迪士尼度假村</view>
+                </view>
+            </view>
+            <view class="right-wrapper">
+                <img class="person-img" src="/static/img/wangyibo.jpg" mode="scaleToFill">
+            </view>
+        </view>
+
+        <view class="info-wrapper">
+            <view class="field">
+                <view class="label">电话</view>
+                <view class="value">{{ phoneNum|empty }}</view>
+                <image class="image-48" src="/static/icon/phone-color.png"></image>
+            </view>
+            <view class="field">
+                <view class="label">微信</view>
+                <view class="value">wcp0923</view>
+                <image class="image-48" src="/static/icon/wechat.png"></image>
+            </view>
+            <view class="field">
+                <view class="label">邮箱</view>
+                <view class="value">1219852916@qq.com</view>
+                <image class="image-48" src="/static/icon/letter.png"></image>
+            </view>
+            <view class="field">
+                <view class="label">地址</view>
+                <view class="value">上海迪士尼度假村</view>
+                <image class="image-48" src="/static/icon/position.png"></image>
+            </view>
+        </view>
+
+        <view class="btn-wrapper" @click="doSavePhone">保存到手机</view>
     </view>
 </template>
-<script lang="ts" src=".
+<script lang="ts" src="./Home.ts"></script>
+<style lang="scss">
+.home-page-wrapper {
+    padding: 24rpx;
+    width: 100%;
+    box-sizing: border-box;
+    .card-wrapper {
+        height: 400rpx;
+        background-color: #c0c0c0;
+        border-radius: 10rpx;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        box-shadow: 4rpx 4rpx 4rpx 0rpx #ccc;
+        .left-wrapper {
+            flex: 1;
+            height: 400rpx;
+            background-color: #3F536E;
+            position: relative;
+            &::before {
+                position: absolute;
+                content: '';
+                width: 0;
+                height: 0;
+                z-index: 0;
+                right: -118rpx;
+                top: 0;
+                bottom: 0;
+                border-right: 120rpx solid transparent;
+                border-bottom: 400rpx solid #3F536E;
+            }
+
+            .name {
+                font-size: 50rpx;
+                color: #ffffff;
+                margin-top: 60rpx;
+                margin-left: 30rpx;
+            }
+            .career {
+                font-size: 38rpx;
+                color: #ffffff;
+                margin-top: 16rpx;
+                margin-left: 30rpx;
+                font-weight: 400;
+            }
+            .phone {
+                margin-top: 60rpx;
+            }
+            .address {
+                margin-top: 10rpx;
+            }
+            .phone, .address {
+                display: flex;
+                height: 36rpx;
+                align-items: center;
+                margin-left: 30rpx;
+                .text {
+                    margin-left: 10rpx;
+                    font-size: 28rpx;
+                    color: #ffffff;
+                }
+            }
+        }
+        .right-wrapper {
+            flex: 1;
+            display: flex;
+            justify-content: flex-end;
+            height: 400rpx;
+            background-color: #FFFFFF;
+            .person-img {
+                width: 100%;
+                height: 400rpx;
+            }
+        }
+    }
+    .info-wrapper {
+        margin-top: 40rpx;
+        padding-left: 24rpx;
+        .label {
+            font-size: 36rpx;
+        }
+        .field {
+            height: 100rpx;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 2rpx solid #f2f2f2;
+            font-size: 32rpx;
+        }
+    }
+    .btn-wrapper {
+        border-radius: 8rpx;
+        height: 80rpx;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #ffffff;
+        font-size: 32rpx;
+        background-color: #24a19c;
+        margin-top: 48rpx;
+    }
+}
+</style>
